49c19e2ccf34f4e63aa9edae9d203660cec0b6c2
Devananda Jyothi <devanandajyothi2005@gmail.com>
2025-04-08 23:22:51 +0530

    lfu modified

--- lfu.c
+++ lfu.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
-int findLFU(int freq[], int frames) {
-    int min = 9999, minIndex = -1;
-    for (int i = 0; i < frames; i++) {
-        if (freq[i] < min) {
-            min = freq[i];
-            minIndex = i;
+int findLFU(int freq[], int time[], int frames) {
+    int minFreq = freq[0], minTime = time[0], pos = 0;
+    for (int i = 1; i < frames; i++) {
+        if (freq[i] < minFreq || (freq[i] == minFreq && time[i] < minTime)) {
+            minFreq = freq[i];
+            minTime = time[i];
+            pos = i;
         }
     }
-    return minIndex;
+    return pos;
 }
 
 int main() {
@@ -24,13 +25,14 @@ int main() {
     for (int i = 0; i < n; i++)
         scanf("%d", &pages[i]);
 
-    int memory[frames], freq[frames];
+    int memory[frames], freq[frames], time[frames];
     for (int i = 0; i < frames; i++) {
         memory[i] = -1;
         freq[i] = 0;
+        time[i] = 0;
     }
 
-    int pagefault = 0;
+    int pagefault = 0, currentTime = 0;
 
     for (int i = 0; i < n; i++) {
         int found = 0;
@@ -38,15 +40,13 @@ int main() {
         for (int j = 0; j < frames; j++) {
             if (memory[j] == pages[i]) {
                 found = 1;
-                freq[j]++;  
+                freq[j]++;
                 break;
             }
         }
 
-
         if (!found) {
             int pos = -1;
-
 
             for (int j = 0; j < frames; j++) {
                 if (memory[j] == -1) {
@@ -55,14 +55,13 @@ int main() {
                 }
             }
 
-            //full case
             if (pos == -1) {
-                pos = findLFU(freq, frames);
+                pos = findLFU(freq, time, frames);
             }
 
-            // space available case
             memory[pos] = pages[i];
             freq[pos] = 1;
+            time[pos] = currentTime++;
             pagefault++;
         }
 
